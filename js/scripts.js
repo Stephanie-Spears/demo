639849edfdcbc86c4eefd5c2ec68e1dfa0151e22
ss <Guest@epicodus-7.local>
2017-01-26 11:52:01 -0800

    test

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
-var Pizza = function(size) {
+var Pizza = function() {
   this.toppings = []
-  this.pizzaSize = size;
-  this.price = 15;
+  this.pizzaSize = ;
+  this.price = 1;
 }
 var toppings = [
   ['cheese', 1.5],
@@ -12,21 +12,37 @@ var toppings = [
   ['ham', 2.0],
   ['pineapple', 2.0]
 ];
-alert(toppings[1][1]);
 
-Pizza.prototype.getToppingPrice(number) {
+
+Pizza.prototype.getToppingPrice = function(number) {
   this.price += topping[number][1];
 }
-Pizza.prototype.writePizzaSpecs() {
+Pizza.prototype.getSizePrice = function(number) {
+  switch (number) {
+    case 0:
+      this.price += 10;
+      break; //small size
+    case 1:
+      this.price += 12;
+      break;
+    case 2:
+      this.price += 14;
+      break;
+    case 3:
+      this.price += 18;
+      break; //XL size
+    default:
+      alert("there is an error in size section");
+  }
+}
+
+Pizza.prototype.writePizzaSpecs = function() {
   var str = "<h5>" + "</h5><br><ul>"
   for (var i = 0; i < this.toppings.length; i++) {
     str = str + "<li>" + this.toppings[i][0] + "</li>";
   }
   str += "</ul>"
   return str;
-}
-Order.prototype.getToppingName(number) {
-  this.price += topping[number][0];
 }
 
 
@@ -35,20 +51,40 @@ var Order = function() {
   this.totalPrice = 0;
 }
 //
-Order.prototype.calcPrice = function(myOrder) {
-  var myArray = myOrder.pizzaArray;
+Order.prototype.calcPrice = function() {
+  var myArray = this.pizzaArray;
   for (var i = 0; i < myArray; i++) {
-    myOrder.totalPrice += myArray[i].price;
+    this.totalPrice += myArray[i].price;
   }
 };
-var testp = new Pizza(1);
-var test = new Order();
+var p1 = new Pizza();
+var p2 = new Pizza();
 
-test.pizzaArray.push(testp);
-test.calcPrice(test);
+var o1 = new Order();
 
-alert("testp: " + testp)
-alert("test.pizzaArray: " + test.pizzaArray);
-alert("test: " + test);
-alert("test.totalPrice: " + test.totalPrice);
-test.totalPrice);
+o1.pizzaArray.push(p1);
+o1.pizzaArray.push(p2);
+
+o1.calcPrice();
+alert(o1.totalPrice)
+
+$(document).ready(function() {
+
+
+    }
+
+
+
+
+    //
+    // var testp = new Pizza(1);
+    // var test = new Order();
+    //
+    // test.pizzaArray.push(testp);
+    // test.calcPrice(test);
+    //
+    // alert("testp: " + testp)
+    // alert("test.pizzaArray: " + test.pizzaArray);
+    // alert("test: " + test);
+    // alert("test.totalPrice: " + test.totalPrice);
+    // test.totalPrice);
